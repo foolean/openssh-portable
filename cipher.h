734e4273c0480d48c4a9d91e9600a6deb39d8dd7
Michael Zhang <mzhang3@kilo.psc.edu>
2022-02-07 23:37:47 -0500

    patch stock V_8_5_P1 with Michael's HPNSSH changes

--- cipher.h
+++ cipher.h
@@ -68,6 +68,7 @@ u_int	 cipher_seclen(const struct sshcipher *);
 u_int	 cipher_authlen(const struct sshcipher *);
 u_int	 cipher_ivlen(const struct sshcipher *);
 u_int	 cipher_is_cbc(const struct sshcipher *);
+void cipher_set_auth_state(int);
 
 u_int	 cipher_ctx_is_plaintext(struct sshcipher_ctx *);
 
